1170dd9b9c4fe73e9ac782d649481452834a9f19
Promod <contactpromods@gmail.com>
2025-12-12 02:14:10 +0530

    Sales Test

--- src/test/java/com/ims/tests/SalesTest.java
+++ src/test/java/com/ims/tests/SalesTest.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 import java.time.Duration;
 
 public class SalesTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
@@ -21,8 +21,29 @@ public class SalesTest {
 
         String salesText= driver.findElement(By.xpath("//a[text()='Sales']")).getText();
         Assert.assertEquals(salesText, "SALES");
+        System.out.println(salesText);
 
+        String billingText= driver.findElement(By.xpath("//h2[contains(text(),'Billing')]")).getText();
+        Assert.assertEquals(billingText,"Multi-Product Sales / Billing");
+        System.out.println(billingText);
 
+        driver.findElement(By.xpath("//div[text()='Select Product']")).click();
+        driver.findElement(By.xpath("//li[text()='Select Product']/following-sibling::li[1]")).click();
+        driver.findElement(By.xpath("//span[text()='Qty']/parent::legend/parent::fieldset/preceding-sibling::input")).sendKeys("1");
+
+//        driver.findElement(By.id("_r_2l_")).sendKeys("1");
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
+
+        Thread.sleep(3000);
+
+        String productText= driver.findElement(By.xpath("//th[text()='Product']")).getText();
+        Assert.assertEquals(productText, "Product");
+
+        driver.findElement(By.xpath("//button[text()='Complete Sale']")).click();
+
+        Thread.sleep(3000);
+        String billText= driver.findElement(By.xpath("//h3[text()='Bill / Receipt']")).getText();
+        Assert.assertEquals(billText, "Bill / Receipt");
 
         driver.quit();
 
